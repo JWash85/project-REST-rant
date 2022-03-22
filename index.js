c4d6d596a5f554d67310e5dd07fcab8f17ee0bfb
Jarmarr Washington <jarmarrwashington@Jarmarrs-iMac.local>
2022-03-22 19:31:46 -0400

    Adding 404 page route

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+//Get the enviroment variables
+require('dotenv').config()
+//require needed node modules
 const express = require('express')
 const app = express()
 
 app.get('/', function(req, res){
     res.send('Hello world!')
 })
-app.listen(3000)
+
+//Make a wildcard route to display 404 error
+//Using arrow function
+app.get('*', (req, res) => {
+    //.status(404) gives a legitmate 404 status code
+    res.status(404).send('<h1>404 Page</h1>')
+})
+//Pulls port variable from .env file
+app.listen(process.env.PORT)
