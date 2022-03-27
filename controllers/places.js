3d600bb511c446da0700c7760d33cbdd94dc7174
Jarmarr Washington <jarmarrwashington@Jarmarrs-iMac.local>
2022-03-27 09:07:35 -0400

    New places can be added and state dropdown list

--- controllers/places.js
+++ controllers/places.js
@@ -2,21 +2,10 @@
 
 //code to create and export an express.Router()
 const router = require('express').Router()
+const places = require('../models/places.js')
+
 router.get('/', (req, res) => {
     //res.send('GET /places')
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/steaming-pizza.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/salad.jpg'
-      }]
     res.render('places/index', {places})
 })
 
@@ -26,6 +15,19 @@ router.get('/new', (req, res) => {
 
 router.post('/', (req, res) => {
     console.log(req.body)
-    res.send('POST /places')
-})
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    //Using push() method to add new places data from req.body to places array
+    places.push(req.body)
+    //redirect used to redirect to another route
+    res.redirect('/places')
+  })
 module.exports = router
