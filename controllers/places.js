671b3d3f3a40cbe154b737b795eeb4868be83670
Jarmarr Washington <jarmarrwashington@Jarmarrs-iMac.local>
2022-03-30 19:17:36 -0400

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -30,4 +30,16 @@ router.post('/', (req, res) => {
     //redirect used to redirect to another route
     res.redirect('/places')
   })
+  router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      res.render('places/show', {place: places[id]})
+    }
+  })
 module.exports = router
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -3,10 +3,14 @@ const Def = require("../default");
 
 function index(data) {
   //Formatting data in place array into HTML so web page can display it.
-  let placesFormatted = data.places.map((place) => {
+  let placesFormatted = data.places.map((place, index) => {
     return (
       <div className="col-sm-6">
-        <h2>{place.name}</h2>
+        <h2>
+          <a href={`/places/${index}`}>
+          {place.name}
+          </a>
+        </h2>
         <p className="text-center">{place.cuisines}</p>
         <img src={place.pic} alt={place.name} />
         <p className="text-center">
