752fa5d6357d08e7145435463b993eb71cc916ea
Jarmarr Washington <jarmarrwashington@Jarmarrs-iMac.local>
2022-03-31 19:47:14 -0400

    Added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -55,4 +55,16 @@ router.post('/', (req, res) => {
       res.redirect('/places')
     }
   })
+  router.get('/:id/edit', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+      res.render('places/edit', { place: places[id] })
+    }
+  })
 module.exports = router
