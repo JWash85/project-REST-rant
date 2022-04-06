8639028b837e7860e62adf7d78f1bd1dcf48771a
Jarmarr Washington <jarmarrwashington@Jarmarrs-iMac.local>
2022-04-05 20:01:13 -0400

    Added Validation Error

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -3,11 +3,20 @@
 const React = require("react");
 const Def = require("../default");
 
-function new_form() {
+function new_form(data) {
+  let message = data.message
+  if (data.message) {
+    message = (
+      <h4 className="alert-danger">
+        {data.message}
+      </h4>
+    )
+  }
   return (
     <Def>
       <main>
         <h1>Add a New Place</h1>
+        {message}
         <form method="POST" action="/places">
         <div className="row">
           <div className="form-group col-sm-6 col-md-6 col-lg-6"> 
@@ -92,7 +101,7 @@ function new_form() {
           </div>
           <div className="form-group col-sm-6 col-md-6 col-lg-6">
             <label for="founded">Founded Year</label>
-            <input className="form-control" id="founded" name="founded" />
+            <input className="form-control" id="founded" name="founded" value={new Date().getFullYear()}/>
           </div>
           </div>
           <input className="btn btn-primary" type="submit" value="Add Place" />
