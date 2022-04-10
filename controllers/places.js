d6c9fbec9732524f0d0c407931f2d546bf4135e5
Jarmarr Washington <jarmarrwashington@Jarmarrs-iMac.local>
2022-04-10 19:41:29 -0400

    Delete and Edit

--- controllers/places.js
+++ controllers/places.js
@@ -55,18 +55,40 @@ router.get('/:id', (req, res) => {
       res.render('error404')
   })
 })
-
+//EDIT ROUTE
 router.put('/:id', (req, res) => {
-  res.send('PUT /places/:id stub')
+  db.Place.findByIdAndUpdate(req.params.id, req.body)
+  .then(() => {
+      res.redirect(`/places/${req.params.id}`)
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
 })
-
+//DELETE ROUTE
 router.delete('/:id', (req, res) => {
-  res.send('DELETE /places/:id stub')
+  db.Place.findByIdAndDelete(req.params.id)
+  .then(() => {
+      res.redirect('/places')
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
 })
 
+//EDIT FORM ROUTE
 router.get('/:id/edit', (req, res) => {
-  res.send('GET edit form stub')
+  db.Place.findById(req.params.id)
+  .then(place => {
+      res.render('places/edit', { place })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
 })
+
 //COMMENT ROUTE
 router.post('/:id/comment', (req, res) => {
   console.log(req.body)
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,39 +1,31 @@
 const React = require("react");
 const Def = require("../default");
 
-function show (data) {
-  let comments = (
-    <h3 className="inactive">
-      No comments yet!
-    </h3>
-  )
-  let rating = (
-    <h3 className="inactive">
-      Not yet rated
-    </h3>
-  )
+function show(data) {
+  let comments = <h3 className="inactive">No comments yet!</h3>;
+  let rating = <h3 className="inactive">Not yet rated</h3>;
   if (data.place.comments.length) {
     let sumRatings = data.place.comments.reduce((tot, c) => {
-      return tot + c.stars
-    }, 0)
-    let averageRating = sumRatings / data.place.comments.length
-    rating = (
-      <h3>
-        {Math.round(averageRating)} stars
-      </h3>
-    )
-    comments = data.place.comments.map(c => {
+      return tot + c.stars;
+    }, 0);
+    let averageRating = Math.round(sumRatings / data.place.comments.length);
+    let stars = "";
+    for (let i = 0; i < averageRating; i++) {
+      stars += "⭐️";
+    }
+    rating = <h3>{stars} stars</h3>;
+    comments = data.place.comments.map((c) => {
       return (
         <div className="border">
-          <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
+          <h2 className="rant">{c.rant ? "Rant! 😡" : "Rave! 😻"}</h2>
           <h4>{c.content}</h4>
           <h3>
             <stong>- {c.author}</stong>
           </h3>
           <h4>Rating: {c.stars}</h4>
         </div>
-      )
-    })
+      );
+    });
   }
   return (
     <Def>
@@ -45,18 +37,14 @@ function show (data) {
           {data.place.cuisines}
         </h3>
         <h3>Rating</h3>
-          {rating}
+        {rating}
         <h3>Description</h3>
-        <h3>
-          {data.place.showEstablished()}
-        </h3>
-        <h4>
-          Serving {data.place.cuisines}
-        </h4>
+        <h3>{data.place.showEstablished()}</h3>
+        <h4>Serving {data.place.cuisines}</h4>
         <a href={`/places/${data.id}/edit`} className="btn btn-warning">
           Edit
         </a>
-        <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+        <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}>
           <button type="submit" className="btn btn-danger">
             Delete
           </button>
@@ -64,28 +52,50 @@ function show (data) {
         <h3>Comments</h3>
         {comments}
         <form action={`/places/${data.place.id}/comment`} method="POST">
-              <div className="row">
-                <div className="form-group col-sm-12">
-                  <label htmlFor="content">Content</label>
-                  <textarea id="content" name="content" className="form-control"></textarea>
-                </div>
-              </div>
-              <div className="row">
-                <div className="form-group col-sm-4">
-                  <label htmlFor="author">Author</label>
-                  <input id="author" name="author" className="form-control" />
-                </div>
-                <div className="form-group col-sm-4">
-                  <label htmlFor="stars">Star Rating</label>
-                  <input type="range" class="form-range" step="0.5" min="1" max="5" id="stars" name="stars" className="form-control" />
-                </div>
-                <div className="form-group col-sm-2">
-                  <label htmlFor="rant">Rant?</label>
-                  <input type="checkbox" id="rant" name="rant" className="form-control" />
-                </div>
-              </div>
-              <input type="submit" className="btn btn-primary" value="Add Comment" />
-            </form>
+          <div className="row">
+            <div className="form-group col-sm-12">
+              <label htmlFor="content">Content</label>
+              <textarea
+                id="content"
+                name="content"
+                className="form-control"
+              ></textarea>
+            </div>
+          </div>
+          <div className="row">
+            <div className="form-group col-sm-4">
+              <label htmlFor="author">Author</label>
+              <input id="author" name="author" className="form-control" />
+            </div>
+            <div className="form-group col-sm-4">
+              <label htmlFor="stars">Star Rating</label>
+              <input
+                type="range"
+                className="form-range"
+                step="0.5"
+                min="1"
+                max="5"
+                id="stars"
+                name="stars"
+                className="form-control"
+              />
+            </div>
+            <div className="form-group col-sm-2">
+              <label htmlFor="rant">Rant?</label>
+              <input
+                type="checkbox"
+                id="rant"
+                name="rant"
+                className="form-control"
+              />
+            </div>
+          </div>
+          <input
+            type="submit"
+            className="btn btn-primary"
+            value="Add Comment"
+          />
+        </form>
       </main>
     </Def>
   );
